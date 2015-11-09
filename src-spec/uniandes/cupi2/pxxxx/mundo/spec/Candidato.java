9f77d5e5efbed93ba8fb22ca7fd07570b72c173e
maveme <help@genmymodel.com>
2015-11-09 16:06:50 +0000

    commit by maveme using genmymodel: v20151109_160648

---
+++ src-spec/uniandes/cupi2/pxxxx/mundo/spec/Candidato.java
@@ -0,0 +1,56 @@
+package uniandes.cupi2.pxxxx.mundo.spec;
+
+import co.edu.uniandes.cupi2.generador.anotaciones.*;
+
+
+public class Candidato extends _Candidato
+{
+	@Atributo(genGet=true, genSet=true)
+	private int edad;
+	@Atributo(genGet=true, genSet=true)
+	private String nombre;
+	@Atributo(genGet=true, genSet=true)
+	private int votos;
+	@Atributo(genGet=true, genSet=true)
+	private String apellido;
+	@Atributo(genGet=true, genSet=true)
+	private double costoCampanha;
+	@Atributo(genGet=true, genSet=true)
+	private String partidoPolitico;
+	
+
+	
+
+	public void agregarVotoRadio()
+	{
+	
+	}
+	
+	public void reiniciarConteoVotos()
+	{
+	
+	}
+	
+	public void agregarVotoTelevision()
+	{
+	
+	}
+	
+	public void reiniciarCostoCampanha()
+	{
+	
+	}
+	
+	public void ingresarUnVoto()
+	{
+	
+	}
+	
+	public void agregarVotoInternet()
+	{
+	
+	}
+	
+	
+}
+
---
+++ src-spec/uniandes/cupi2/pxxxx/mundo/spec/Urna.java
@@ -0,0 +1,95 @@
+package uniandes.cupi2.pxxxx.mundo.spec;
+
+import co.edu.uniandes.cupi2.generador.anotaciones.*;
+
+
+@Mundo
+public class Urna extends _Urna
+{
+	
+
+	private Candidato candidato1;
+	
+	private Candidato candidato3;
+	
+	private Candidato candidato2;
+	
+	
+
+	public void ingresarVotoRadioCandidato3()
+	{
+	
+	}
+	
+	public void inicializar()
+	{
+	
+	}
+	
+	public void ingresarVotoTelevisionCandidato3()
+	{
+	
+	}
+	
+	public void ingresarVotoInternetCandidato3()
+	{
+	
+	}
+	
+	public void ingresarVotoRadioCandidato1()
+	{
+	
+	}
+	
+	public double calcularPorcentajeVotosCandidato3()
+	{
+	
+	}
+	
+	public void ingresarVotoInternetCandidato1()
+	{
+	
+	}
+	
+	public double calcularPorcentajeVotosCandidato2()
+	{
+	
+	}
+	
+	public void ingresarVotoTelevisionCandidato2()
+	{
+	
+	}
+	
+	public void ingresarVotoTelevisionCandidato1()
+	{
+	
+	}
+	
+	public double calcularCostoPromedioCampanha()
+	{
+	
+	}
+	
+	public void ingresarVotoInternetCandidato2()
+	{
+	
+	}
+	
+	public void ingresarVotoRadioCandidato2()
+	{
+	
+	}
+	
+	public int calcularTotalVotos()
+	{
+	
+	}
+	
+	public double calcularPorcentajeVotosCandidato1()
+	{
+	
+	}
+	
+	
+}
